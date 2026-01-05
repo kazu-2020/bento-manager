bafcb870ea6bc030ba848a8372eaa6d4b359caee
kazu <64774307+kazu-2020@users.noreply.github.com>
2026-01-05 18:03:53 +0900

    スタイル: available_localesの配列のスペースを調整

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module BentoManager
 
     # デフォルトロケールを日本語に設定
     config.i18n.default_locale = :ja
-    config.i18n.available_locales = [:ja, :en]
+    config.i18n.available_locales = [ :ja, :en ]
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
