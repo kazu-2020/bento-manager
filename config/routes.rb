e61cc4be645e41913f6eda16360a30ae22663143
kazu <64774307+kazu-2020@users.noreply.github.com>
2026-01-24 09:31:59 +0900

    Fix formatting of resource declaration in routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
   # 共有リソース（Admin と Employee 両方がアクセス可能）
   resources :locations, except: [ :destroy ]
   namespace :catalogs do
-    resource :form_fields, only: [:show]
+    resource :form_fields, only: [ :show ]
   end
   resources :catalogs
 
