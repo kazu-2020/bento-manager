3fde29c1e4b0267323857666d3074c406deeca32
kazu <64774307+kazu-2020@users.noreply.github.com>
2026-04-26 12:52:23 +0900

    feat: クロス集計の構成比列をスタックバーに変更し関係者/一般の数値列を削除

--- app/views/components/sales_analyses/cross_table/component.rb
+++ app/views/components/sales_analyses/cross_table/component.rb
@@ -24,6 +24,19 @@ module SalesAnalyses
         return 0 if row[:total_quantity].zero?
         (row[:staff_quantity] * 100.0 / row[:total_quantity]).round
       end
+
+      def max_total
+        @max_total ||= data.map { |r| r[:total_quantity] }.max || 1
+      end
+
+      def bar_total_width(row)
+        "#{(row[:total_quantity] * 100.0 / max_total).round}%"
+      end
+
+      def staff_bar_ratio(row)
+        return "0%" if row[:total_quantity].zero?
+        "#{(row[:staff_quantity] * 100.0 / row[:total_quantity]).round}%"
+      end
     end
   end
 end
